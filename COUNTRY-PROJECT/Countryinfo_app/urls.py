6099a51f6416f3cbe24aca9ca1a360a998e0a710
Abhishek182005 <abhishek2005saxena@gmail.com>
2024-06-30 15:50:32 +0530

    initial

---
+++ COUNTRY-PROJECT/Countryinfo_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('menu/', views.menu, name='menu'),
+    path('country/', views.country_info, name='country_info'),
+    path('compare/', views.country_compare, name='country_compare'),
+    path('capitals/', views.country_and_capitals, name='country_and_capitals'),
+    path('error/', views.error, name='error'),
+    path('', views.landing_page, name='landing_page')
+]
